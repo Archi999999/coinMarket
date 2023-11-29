f9ee33d842827a03e2a8ef7b0b79824b17ed00d7
Sergey <Zaikouski.sergey@gmail.com>
2023-11-29 09:51:16 +0200

    fix: add classname from props to component

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -14,6 +14,9 @@ export const Button = <T extends ElementType = 'button'>(
   const { as: Component = 'button', className, fullWidth, variant = 'primary', ...rest } = props
 
   return (
-    <Component className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''}`} {...rest} />
+    <Component
+      className={`${s.button} ${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      {...rest}
+    />
   )
 }
